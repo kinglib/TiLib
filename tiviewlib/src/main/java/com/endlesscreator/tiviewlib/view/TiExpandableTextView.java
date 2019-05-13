48a22fd7520f8287297c15e3d43fe7d5a06be5a1
tyewang <752448099@qq.com>
2019-05-14 01:53:26 +0800

    添加展开Text的库

---
+++ tiviewlib/src/main/java/com/endlesscreator/tiviewlib/view/TiExpandableTextView.java
@@ -0,0 +1,21 @@
+package com.endlesscreator.tiviewlib.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import com.ctetin.expandabletextviewlibrary.ExpandableTextView;
+
+public class TiExpandableTextView extends ExpandableTextView {
+    public TiExpandableTextView(Context context) {
+        super(context);
+    }
+
+    public TiExpandableTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TiExpandableTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
