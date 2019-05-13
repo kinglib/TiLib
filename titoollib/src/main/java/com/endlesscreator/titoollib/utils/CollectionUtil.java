d8c1fd4006aa40f95964a7d2342b0098d75c0313
tyewang <752448099@qq.com>
2019-05-13 14:42:03 +0800

    添加集合工具类

---
+++ titoollib/src/main/java/com/endlesscreator/titoollib/utils/CollectionUtil.java
@@ -0,0 +1,31 @@
+package com.endlesscreator.titoollib.utils;
+
+import java.util.List;
+import java.util.Map;
+
+public class CollectionUtil {
+
+    public static <T> boolean isEmpty(List<T> aList) {
+        return aList == null || aList.isEmpty();
+    }
+
+    public static <T> T getItem(List<T> aList, int aIndex) {
+        if (aList != null && aIndex >= 0 && aIndex < aList.size()) {
+            return aList.get(aIndex);
+        }
+        return null;
+    }
+
+    public static <K, T> int getItemsSize(Map<K, List<T>> aMap) {
+        int lSize = 0;
+        if (aMap != null && aMap.size() > 0) {
+            List<T> lValues;
+            for (Map.Entry<K, List<T>> lItems : aMap.entrySet()) {
+                lValues = lItems.getValue();
+                if (lValues != null) lSize += lValues.size();
+            }
+        }
+        return lSize;
+    }
+
+}
